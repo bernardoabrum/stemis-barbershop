77da237eabca8196267b83c4978db1c96bca97df
Bernardo <bernardoalvesbrum_100@hotmail.com>
2025-02-02 15:20:57 -0300

    fix: persistance login

--- src/views/authenticate/Authenticate.vue
+++ src/views/authenticate/Authenticate.vue
@@ -4,6 +4,7 @@
       <Login />
       <Register />
     </div>
+    <p>Certifique-se de que o json-server esteja observando o arquivo db.json na porta 3000</p>
     <p>Primeiro crie uma ou mais contas e faça alguns agendamentos</p>
     <p>
       Depois, faça login usando email: "admin" e senha: "admin" para ter acesso
